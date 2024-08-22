84d27c1f50052a669a5d20cd98b4910961ce6b71
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-08-22 14:04:51 -0400

    Added access to static files

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
+// Access static files
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('Hello from middleware 👋');
   //must call next() to pass execution to the next
