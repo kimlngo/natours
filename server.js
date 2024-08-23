75cb0c87210530db98ea0ff0975d3f946bcda793
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-08-23 18:13:54 -0400

    Add connection to Remote/Local MongoDB and remove eslint

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-// console.log(process.env);
-const PORT = process.env.PORT;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+//REMOTE DATABASE
+mongoose.connect(DB).then(() => {
+  console.log('DB Connection Successful');
+});
+
+//LOCAL DATABASE
+// mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+//   console.log('DB Connection Successful');
+// });
+
+const { PORT } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App is up and running on port ${PORT}...`);
