fd1201068c9eb0851a39053a006ccec16a7bcc02
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-08-24 15:55:35 -0400

    Create Mongoose Schema, Model and insert first document

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
+const DB_CONNECTION = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
 );
 
 //REMOTE DATABASE
-mongoose.connect(DB).then(() => {
+mongoose.connect(DB_CONNECTION).then(() => {
   console.log('DB Connection Successful');
 });
 
@@ -19,7 +19,43 @@ mongoose.connect(DB).then(() => {
 //   console.log('DB Connection Successful');
 // });
 
-const { PORT } = process.env;
+//Create Mongoose Schema
+const simpleTourSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  price: Number,
+});
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a unique name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = new mongoose.model('Tour', tourSchema); //convention: to name mongoose model with first capital letter
+
+const testTour = new Tour({
+  name: 'St Joseph Oratory',
+  rating: 4.9,
+  price: 200,
+});
+
+testTour
+  .save()
+  .then(doc => console.log(doc))
+  .catch(err => console.error(err));
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`App is up and running on port ${PORT}...`);
