83bb5d203459fd7bc0365e4790d822306a3eedc2
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-08-24 22:36:01 -0400

    Modelling Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -12,15 +12,53 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a unique name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true, //remove all the whitespace at the beginning and end.
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    //automatically created timestamp
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = new mongoose.model('Tour', tourSchema); //convention: to name mongoose model with first capital letter
