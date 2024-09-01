4150337994087ea5edb555afbcfc64ab4be38218
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-09-01 16:18:06 -0400

    Implement rejected promises and uncaught exception handlers

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+//Handle Uncaught Exception
+process.on('uncaughtException', err => {
+  console.log('UNHANDLED EXCEPTION!!! 💥');
+  console.log(`Error: ${err}`);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 const DB_CONNECTION = process.env.DATABASE.replace(
@@ -21,6 +28,17 @@ mongoose.connect(DB_CONNECTION).then(() => {
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App is up and running on port ${PORT}...`);
+});
+
+//Handle un-handled rejected promises
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED EXCEPTION!!! 💥');
+  console.log(`Error Name: ${err.name} - Error Message: ${err.message}`);
+
+  //close the server gracefully
+  server.close(() => {
+    process.exit(1); //eventually kill the process
+  });
 });
